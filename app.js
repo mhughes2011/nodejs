f21510c972449b5a0b40edc6cb4086b1d9ae1fc2
M Hughes <mhughes2011@gmail.com>
2021-04-27 18:52:33 -0700

    added user ability to request names on CLI

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 //Require https module.  This is needed for the get request when connecting to the API.
 const https = require('https');
-const username = "maxhughes2";
 
 //Function to print message to console.
 function printMessage(username, badgeCount, point) {
@@ -11,20 +10,36 @@ function printMessage(username, badgeCount, point) {
   console.log(message);
 }
 
-//Connect to the API URL (https://teamtreehouse.com/<username>.json)
 
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  let body = "";
-  //Read the data
-  response.on('data', data => {
-    body += data.toString();
+function getProfile(username) {
+  //Connect to the API URL (https://teamtreehouse.com/<username>.json)
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    let body = "";
+    //Read the data when it loads, hence the on.(data) method.  JSON data is delivered in chunks so it keeps adding data to the body variable as it loads in the form of a string.
+    response.on('data', data => {
+      body += data.toString();
+    });
+    
+    //Once all of the data is loaded it parses it and displays it to the console.
+    response.on('end', () => {
+      //Parse the data because it's a string
+      let profile = JSON.parse(body);
+      //Print the data from the API
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+    });
   });
-  
-  response.on('end', () => {
-    //Parse the data because it's a string
-    let profile = JSON.parse(body);
-    //Print the data from the API
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
-  });
+}
+
+//To enable the user to add the names it wants to grab at the end of the node app.js command it's...
+
+const users = process.argv.slice(2);
+
+//const users = ['chalkers', 'maxhughes2', 'davemcfarland'];
+
+users.forEach(username => {
+  getProfile(username);
 });
 
+//The above code can be shortened to the below because it's only passing in one variable.
+//
+//users.forEach(getProfile);
